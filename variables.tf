3a664ff50ccb5c06111d58b644a9b615335088f3
LolaNeh <loisekmokua@gmail.com>
2023-09-21 22:02:49 -0500

    terraform

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+  description = "Amazon machine image"
+  type = string
+  default = "ami-00c6177f250e07ec1"
+}
+variable "instanceType" {
+    default = "t2.small"
+  
+}
+variable "region_name" {
+    #default = "us-east-1"
+  
+}
+variable "profile_name" {
+    default = "default"
+  
+}
